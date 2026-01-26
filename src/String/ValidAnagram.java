7e5ded61c0b2e537d438558f8b6629bb70e8547d
Shubham Devghare <shubhamkd.a02@gmail.com>
2026-01-26 23:43:46 +0530

    init commit

--- src/String/ValidAnagram.java
+++ src/String/ValidAnagram.java
@@ -14,19 +14,20 @@ Output: false
  */
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        if (s.length() != t.length()) {    // If lengths differ → not an anagram
             return false;
         }
 
         int[] count = new int[26];
 
+       //  Count each character
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
 
         for (int c : count) {
-            if (c != 0) {
+            if (c != 0) {         //If any count not equals Zero → not an anagram
                 return false;
             }
         }
