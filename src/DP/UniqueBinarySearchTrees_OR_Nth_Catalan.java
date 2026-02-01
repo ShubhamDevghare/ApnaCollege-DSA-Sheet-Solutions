3b809aac2778341e51944f90f9234384d2ee1763
Shubham Devghare <shubhamkd.a02@gmail.com>
2026-02-01 09:26:54 +0530

    init commit

---
+++ src/DP/UniqueBinarySearchTrees_OR_Nth_Catalan.java
@@ -0,0 +1,18 @@
+package DP;
+
+public class UniqueBinarySearchTrees_OR_Nth_Catalan {
+    public int numTrees(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+
+        return dp[n];
+    }
+}
--- src/StriverDSA_DynamicPrograming/NinjaTraining.java
+++ src/StriverDSA_DynamicPrograming/NinjaTraining.java
@@ -1,7 +1,28 @@
 package StriverDSA_DynamicPrograming;
 
 import java.util.*;
+/*
+Ninja is planning this ‘N’ days-long training schedule.
+Each day, he can perform any one of these three activities: Running, Fighting Practice or Learning New Moves.
+Each activity has some merit points on each day.
+As Ninja has to improve all his skills, he can’t do the same activity in two consecutive days.
+Can you help Ninja find out the maximum merit points Ninja can earn?
 
+You are given a 2D array of size N*3 ‘POINTS’ with the points corresponding to each day and activity.
+Your task is to calculate the maximum number of merit points that Ninja can earn.
+ */
+/*
+🥷 Problem in simple words
+===========================
+Ninja trains for N days
+Each day he can do one of 3 activities:
+0 → Running
+1 → Fighting Practice
+2 → Learning New Moves
+points[day][activity] gives merit points
+Constraint: He cannot repeat the same activity on two consecutive days
+Goal: maximize total merit points
+ */
 public class NinjaTraining {
 
     // Recursive function to calculate the maximum points
